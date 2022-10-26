d8717721ae95cfb52f6b41f790b7f62ad3ffcd7f
Raghdaa99 <roroter999@gmail.com>
2022-10-27 00:23:12 +0300

    website-change

--- website-1/js/script.js
+++ website-1/js/script.js
@@ -4,6 +4,9 @@ let heightHireo = document.querySelector('.intro');
 let header = document.querySelector('header');
 let btn = document.getElementById("btn_top");
 
+let nums = document.querySelectorAll(".stats .box .num");
+let statsSection = document.querySelector(".stats");
+let started = false;
 
 window.onscroll = function () {
 
@@ -19,15 +22,38 @@ window.onscroll = function () {
     scrollTop();
 
     skillsRatio();
-}
 
+
+    // Stats Increase Number
+    if (window.scrollY >= statsSection.offsetTop - statsSection.offsetHeight) {
+        if (!started) {
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;
+    }
+}
+function startCount(el) {
+
+    let numData = el.dataset.num;
+    let counter = setInterval(() => {
+        el.textContent++;
+        console.log(el.textContent + " " + numData);
+        if (el.textContent == numData) {
+            console.log('clear');
+            clearInterval(counter);
+        }
+
+    }, 5);
+}
 
 // Scroll Top  ---------(2)
 function scrollTop() {
-    if (window.scrollY >= 600) {
-        btn.style.display = "block";
+    if (window.scrollY > 721.6) {
+        // btn.style.display = "block";
+        btn.style.bottom = "20px"
     } else {
-        btn.style.display = "none";
+        // btn.style.display = "none";
+        btn.style.bottom = "-80px"
     }
 }
 btn.onclick = function () {
@@ -62,7 +88,7 @@ let fw = document.querySelectorAll('.featured-work ul li');
 let imgDiv = document.querySelectorAll('.shuffle-imgs .image');
 let shuffle_imgs = document.querySelector('.shuffle-imgs');
 
-function setSuffleImages(arr){
+function setSuffleImages(arr) {
     arr.forEach(function (img) {
         shuffle_imgs.appendChild(img);
     });
@@ -74,7 +100,7 @@ fw.forEach(function (li) {
         handelActive(event);
         let li_class_name = event.target.dataset.class;
         if (li_class_name === 'all') {
-           setSuffleImages(imgDiv);
+            setSuffleImages(imgDiv);
         } else {
             const results = [...imgDiv].filter(img => li_class_name === img.children[0].className);
             setSuffleImages(results);
@@ -162,9 +188,9 @@ function validateInputs() {
 // Loading Screen     ---------(6)  
 let loader = document.querySelector('.loader-bg');
 window.addEventListener("load", function () {
-    setTimeout(function () {
-        loader.style.display = "none";
-    }, 1500)
+    // setTimeout(function () {
+    loader.style.display = "none";
+    // }, 1500)
 })
 
 
@@ -172,7 +198,7 @@ window.addEventListener("load", function () {
 let landingPage = document.querySelector(".intro");
 
 //Get Array pf Imgs
-let imgsArray = ["img1.jpg", "img2.jpg", "img3.jpeg", "img4.jpg"];
+let imgsArray = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
 
 function randomizeImgs() {
     setInterval(() => {
@@ -190,12 +216,19 @@ function skillsRatio() {
     let skillsOuterHeight = ourSkills.offsetHeight;
     let windowHeight = this.innerHeight;
     let windowScrollTop = this.pageYOffset;
-    console.log(windowHeight);
+    // console.log(windowHeight);
     if (windowScrollTop > (skillsOffsetTop + skillsOuterHeight - windowHeight)) {
-        console.log("kkk");
+        // console.log("kkk");
         let allSkills = document.querySelectorAll(".skill .skill-progress span");
         allSkills.forEach(skill => {
             skill.style.width = skill.dataset.progress;
         });
     }
 }
+
+let toggle_menu = document.querySelector(".toggle-menu");
+toggle_menu.onclick = function () {
+    let menu_list = document.querySelector(".menu-list");
+    menu_list.classList.toggle('active');
+}
+
