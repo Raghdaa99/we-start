eebca333758ea6f6a6ddce8d7a8e6733697a70af
Raghdaa99 <roroter999@gmail.com>
2022-11-10 20:02:28 +0200

    laravel-news-website

--- backend/news-website/database/migrations/2022_11_05_163409_create_admins_table.php
+++ backend/news-website/database/migrations/2022_11_05_163409_create_admins_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
